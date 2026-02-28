2a19cc52a375c63563d835d9a43bfacd893dae34
LassazVegaz <lasindunuwanga@gmail.com>
2026-03-01 05:16:13 +0800

    store comparison data in DB

--- src/services/compare.service.ts
+++ src/services/compare.service.ts
@@ -1,6 +1,7 @@
-import CallModelResponse from "@/types/model-response.type";
 import { LanguageModel, streamText } from "ai";
 import pricingService from "./pricing.service";
+import prisma from "./prisma.service";
+import { Message } from "@/generated/prisma/client";
 
 type CompareParams = {
   prompt: string;
@@ -13,27 +14,36 @@ export class ComparisonService {
       params.models.map((m) => this.callModel(m, params.prompt)),
     );
 
-    return res;
+    const created = await prisma.compareGroup.create({
+      data: {
+        prompt: params.prompt,
+        responses: res,
+      },
+    });
+
+    return created;
   }
 
   private async callModel(
     model: LanguageModel,
     prompt: string,
-  ): Promise<CallModelResponse> {
+  ): Promise<Message> {
     const startTime = Date.now();
     const res = streamText({ model, prompt });
     const usage = await res.totalUsage;
     const text = await res.text;
     const endTime = Date.now();
 
-    const price = await pricingService.calculateCost(model, usage);
+    const price = await pricingService.getModelPrice(model);
 
     return {
       text,
-      model,
-      price: price?.totalCost,
-      totalTokens: usage.totalTokens,
-      timeTakenMs: endTime - startTime,
+      model: model.toString(),
+      inputTokens: usage.inputTokens ?? null,
+      outputTokens: usage.outputTokens ?? null,
+      inputTokenPrice: price.input ?? null,
+      outputTokenPrice: price.output ?? null,
+      time: endTime - startTime,
     };
   }
 }
