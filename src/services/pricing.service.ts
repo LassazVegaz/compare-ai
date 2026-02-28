105b7b179bb46daffecff383ad1fb69e5d8f2d5b
LassazVegaz <lasindunuwanga@gmail.com>
2026-03-01 05:15:59 +0800

    pricing service simplified

--- src/services/pricing.service.ts
+++ src/services/pricing.service.ts
@@ -1,5 +1,5 @@
 import { gateway, GatewayLanguageModelEntry } from "@ai-sdk/gateway";
-import { LanguageModel, LanguageModelUsage } from "ai";
+import { LanguageModel } from "ai";
 import NodeCache from "node-cache";
 
 /**
@@ -13,53 +13,14 @@ const singleModelCache = new NodeCache({ stdTTL: 60 * 60 });
 const allModelsCache = new NodeCache({ stdTTL: 60 * 1 });
 const CACHE_KEY_ALL_MODELS = "all_models";
 
-type PriceBreakdown = {
-  /**Cost of an input token in USD */
-  inputCost: number;
-  /**Cost of an output token in USD */
-  outputCost: number;
-};
-
-type CalculateCostResults = {
-  /**Total cost in USD */
-  totalCost: number;
-  /**Price breakdown during calculations */
-  breakdown: PriceBreakdown;
-};
-
 export class PricingService {
-  async calculateCost(
-    model: LanguageModel,
-    usage: LanguageModelUsage,
-  ): Promise<CalculateCostResults | undefined> {
-    // If any price details are missing, we cannot calculate the total cost.
-    // hence we return undefined to indicate that the cost cannot be calculated.
-    if (!usage.inputTokens || !usage.outputTokens) return undefined;
-
-    const cost = await this.getModelCost(model);
-    if (!cost || !cost.input || !cost.output) return undefined;
-
-    const inputCost = usage.inputTokens * cost.input;
-    const outputCost = usage.outputTokens * cost.output;
-
-    return {
-      totalCost: inputCost + outputCost,
-      breakdown: {
-        inputCost,
-        outputCost,
-      },
-    };
-  }
-
-  private async getModelCost(model: LanguageModel) {
+  async getModelPrice(model: LanguageModel) {
     const modelInfo = await this.getModelInfo(model);
 
-    if (!modelInfo) return undefined;
-
-    const input = modelInfo.pricing?.input
+    const input = modelInfo?.pricing?.input
       ? Number.parseFloat(modelInfo.pricing.input)
       : undefined;
-    const output = modelInfo.pricing?.output
+    const output = modelInfo?.pricing?.output
       ? Number.parseFloat(modelInfo.pricing.output)
       : undefined;
 
