f80037dfad7dca7dc8e356b8024ded62ed58d8ac
LassazVegaz <lasindunuwanga@gmail.com>
2026-03-01 22:36:13 +0800

    revert prisma workarounds

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  outputFileTracingIncludes: {
-    "/*": ["./node_modules/.prisma/client/**/*"],
-  },
 };
 
 export default nextConfig;
